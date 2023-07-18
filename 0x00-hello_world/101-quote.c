2013890c5bd7e8ee3b9ab0ec2a0c58c9ef2f502a
Nokwazi99 <nokwazinomcebo99@gmail.com>
2023-07-18 04:01:27 -0700

    6-size.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,9 +6,11 @@
  *
  * Return: Always 1 (Success)
  */
+
 int main(void)
 {
 	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, quote, 59);
+	write(2, quote, sizeof(quote) - 1);
+
 	return (1);
 }
